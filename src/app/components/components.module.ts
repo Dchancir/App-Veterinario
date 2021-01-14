6c26bfa03444ff2fa97546956e1ae84d2a965c77
Dchancir <daniel.chancir@perficient.com>
2021-01-14 10:30:52 -0500

    Avance-2

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PacientesComponent } from './pacientes/pacientes.component';
 import { ProductosComponent } from './productos/productos.component';
+import { SearchComponent } from './search/search.component';
+import { PacienteComponent } from './paciente/paciente.component';
 
 
 
 @NgModule({
-  declarations: [PacientesComponent, ProductosComponent],
+  declarations: [PacientesComponent, ProductosComponent, SearchComponent, PacienteComponent],
   imports: [
     CommonModule
   ]
